9aa2a1651a2c4a203d88d735e805a783d45a8ac0
Dorin Moldovan <moldovandorinv@gmail.com>
2020-06-22 21:28:27 +0300

    commit Lab19

---
+++ ArrayHelper.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_19_ArrayHelper
+{
+    class ArrayHelper<T> where T : IEquatable<T>, IComparable<T>
+    {
+        public delegate int[] AdunareSiruriEgale(int[] SirUnu, int[] SirDoi);
+
+        public T FoundItem;
+        public bool ValidOrNot;
+        private List<T> Collection;
+        int i = 0;
+
+        public ArrayHelper(params T[] args)
+        {
+            this.Collection = new List<T>();
+            if (args.Length == 0)
+            {
+                args = new T[0];
+            }
+            this.Collection.AddRange(args);
+        }
+
+        //Metoda de adunare care va fi folosita prin delgatul din Program
+        public int[] AdunareDouaSiruri(int[] SirUnu, int[] SirDoi)
+        {
+            int[] Rezultat = new int[SirUnu.Length];
+            foreach ( int no in SirUnu ) {
+                Rezultat[i] = no + SirDoi[i];
+                i++;
+            }
+            return Rezultat;
+        }
+
+        public bool Finder(T element)
+        {
+            //localizare si intoarcere un element intr-un sir
+            foreach (T item in this.Collection)
+            {
+                if (item.Equals(element))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public dynamic Finder(T element, string decider = null)
+        {
+            bool IsValid = Finder(element);
+          
+            if (IsValid)
+            {
+                if ( decider == "element" )
+                {
+                    return element;
+                } else if ( decider == "index" )
+                    return (short)this.Collection.IndexOf(element);
+            }
+
+            return -1;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab_19_ArrayHelper
+{
+    public delegate T[] AdunareSiruriEgale<T>(T[] SirUnu, T[] SirDoi);
+
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int[] SirUnu = new int[] { 7, 15, 12, 30 };
+            int[] SirDoi = new int[] { 27, 4, 44, 17 };
+            int[] Rezultat = new int[4];
+            int x = 0;
+      
+            ArrayHelper<int> Colectie = new ArrayHelper<int>(40, 18, 25, 10, 30, 28);
+            AdunareSiruriEgale<int> AdunareSiruri = Colectie.AdunareDouaSiruri;
+            Rezultat = AdunareSiruri(SirUnu, SirDoi);
+
+            foreach (int i in Rezultat )
+            {
+                //Se afiseaza rezultatul adunarii celor doua colectii de 4 elemente
+                Console.WriteLine($"Adunare doua siruri, elementul {x + 1}: {i.ToString()}");
+                x++;
+            }
+           // Colectie.Finder(25);
+
+            Predicate<int>FinderItemFirst = Colectie.Finder;
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("Elementul este gasit in sir: " + (FinderItemFirst.Invoke(25) ? "DA" : "NU"));
+            Func<int, string, dynamic> FinderItemSecond = Colectie.Finder;
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("Indexul elementului gasit in sir: " + FinderItemSecond.Invoke(25, "index"));
+            Func<int, string, dynamic> FinderItemThird = Colectie.Finder;
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("Elementul propriu zis gasit in sir: " + FinderItemThird.Invoke(25, "element"));
+        }
+    }
+}
